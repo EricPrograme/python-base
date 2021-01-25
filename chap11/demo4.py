232a3c244a68480f6b8beaca5067347aae367505
hmchen <hmchen@cenboomh.com>
2021-01-25 18:04:17 +0800

    Noissue:异常

---
+++ chap11/demo4.py
@@ -0,0 +1,12 @@
+# --*--coding:utf-8--*--
+
+# author : hmchen
+# email  : hmchen@cenboomh.com
+# date   : 2021/1/25 16:36
+
+
+a = int(input("请输入第一个数字："))
+b = int(input("请输入第二个数字："))
+
+result = a / b
+print('输出的结果为：', result)
