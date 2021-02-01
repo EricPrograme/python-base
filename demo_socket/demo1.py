3724b3e508c241aa61f35e73c47efa06694b73fd
hmchen <hmchen@cenboom.com>
2021-02-01 23:17:48 +0800

    Noissue;socket

---
+++ demo_socket/demo1.py
@@ -0,0 +1,13 @@
+import socket
+
+
+sock = socket.socket()
+sock.bind(('127.0.0.1', 8081))
+sock.listen(5)
+
+
+while True:
+    conn,addr = sock.accept()
+    data = conn.recv(8096)
+    conn.send(b'123123')
+    conn.close()
